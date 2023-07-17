664b36a80c369b09ae66d63f215b2efd19f2a6bf
Suraksha <Suraksha@Ashutosh>
2023-07-17 22:30:39 +0530

    ashutosh commiting code for method ovvverrding 17-7-2023

---
+++ src/com/ashutoshpgm/MethodOverloading.java
@@ -0,0 +1,37 @@
+package com.ashutoshpgm;
+
+public class MethodOverloading {
+	public static void main(String[] args) {
+		AA a1=new AA();
+		 byte b=20;
+		a1.add(b, b);
+		
+		int a=20;
+		float f=a; //Implicit casting
+		
+		float ff=10.0f;
+		int aa=(int)ff; // Explicit casting
+		
+		
+	}
+
+}
+
+class AA{
+	void add(int a,int b){
+		System.out.println("squence 1");
+		
+	}
+	float add(int a,int b,int c) {
+		System.out.println("differeent number of parameter");
+		return 0;
+	}
+	double add(float b, int a) {
+		System.out.println("seq");
+		return 0;
+	}
+	float add(int a, float b) {
+		System.out.println("different sequence");
+		return 0;
+	}
+}
